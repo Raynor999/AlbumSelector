e1d43a73ee02647910f77f50f091b3b040fce019
lijunguan <731212914@qq.com>
2016-05-03 18:00:59 +0800

    添加Readme.md 修改sample

--- sample/src/main/java/io/github/lijunguan/sample/MainActivity.java
+++ sample/src/main/java/io/github/lijunguan/sample/MainActivity.java
@@ -1,8 +1,11 @@
 package io.github.lijunguan.sample;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.RadioGroup;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,10 +47,15 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.et_span_count)
     EditText mEtSpanCount;
 
+    @Bind(R.id.rl_avator)
+    View mRlAvator;
+    @Bind(R.id.iv_avator)
+    ImageView mIvAvator;
+
+
+    boolean isAvatorModel;
 
     private SelectedImgAdapter mAdapter;
-
-    private List<String> mImagePaths = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,9 +82,11 @@ public class MainActivity extends AppCompatActivity {
         switch (mRadioGroupSelectModel.getCheckedRadioButtonId()) {
             case R.id.radio_multi:
                 imageSelector.setSelectModel(ImageSelector.MULTI_MODE);
+                isAvatorModel = false;
                 break;
             case R.id.radio_avator:
                 imageSelector.setSelectModel(ImageSelector.AVATOR_MODE);
+                isAvatorModel = true;
                 break;
             default:
                 imageSelector.setSelectModel(ImageSelector.MULTI_MODE);
@@ -117,23 +128,43 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.unbind(this);
     }
 
+    /**
+     * 得到选择的图片路径集合
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == ImageSelector.REQUEST_SELECT_IMAGE) {
             if (resultCode == RESULT_OK) {
-                mImagePaths = data.getStringArrayListExtra(ImageSelector.SELECTED_RESULT);
-                if (mImagePaths != null) {
-                    mAdapter.notifyDataSetChanged();
-                    for (String mImagePath : mImagePaths) {
-                        KLog.d(mImagePath);
-                    }
+                ArrayList<String> imagesPath = data.getStringArrayListExtra(ImageSelector.SELECTED_RESULT);
+                if (isAvatorModel && imagesPath != null) {
+                    mRlAvator.setVisibility(View.VISIBLE);
+                    Glide.with(this)
+                            .load(imagesPath.get(0))
+                            .asBitmap()
+                            .into(new BitmapImageViewTarget(mIvAvator) {
+                                @Override
+                                protected void setResource(Bitmap resource) {
+                                    RoundedBitmapDrawable circularBitmapDrawable =
+                                            RoundedBitmapDrawableFactory.create(getResources(), resource);
+                                    circularBitmapDrawable.setCircular(true);
+                                    mIvAvator.setImageDrawable(circularBitmapDrawable);
+                                }
+                            });
+
+                } else if (imagesPath != null) {
+                    mRlAvator.setVisibility(View.GONE);
+                    mAdapter.refreshData(imagesPath);
                 }
+
             }
         }
     }
 
     class SelectedImgAdapter extends RecyclerView.Adapter<SelectedImgAdapter.ImgViewHolder> {
+
+        List<String> mData = new ArrayList<>();
+
         @Override
         public ImgViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             ImageView imageView = new ImageView(MainActivity.this);
@@ -145,13 +176,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(ImgViewHolder holder, int position) {
             Glide.with(MainActivity.this)
-                    .load(mImagePaths.get(position))
+                    .load(mData.get(position))
                     .into((ImageView) holder.itemView);
         }
 
         @Override
         public int getItemCount() {
-            return mImagePaths.size();
+            return mData.size();
         }
 
         class ImgViewHolder extends RecyclerView.ViewHolder {
@@ -159,6 +190,11 @@ public class MainActivity extends AppCompatActivity {
             public ImgViewHolder(View itemView) {
                 super(itemView);
             }
+        }
+
+        public void refreshData(List<String> data) {
+            mData = data;
+            notifyDataSetChanged();
         }
     }
 
